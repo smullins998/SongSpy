432e11d19169ad2dd42cc6e98fb27fcd1256bcc3
Sean Mullins <Smullins998@gmail.com>
2023-06-05 13:17:15 -0400

    Optimize Shazam

--- Shazam.py
+++ Shazam.py
@@ -6,11 +6,10 @@ import time
 def ShazamSong(file_path):
     audio = AudioSegment.from_file(file_path)
     start_time = 30000  
-    end_time = len(audio)  
+    end_time = len(audio)
     trimmed_audio = audio[start_time:end_time]
-    trimmed_audio.export(file_path)
 
-    mp3_file_content_to_recognize = open(file_path, 'rb').read()
+    mp3_file_content_to_recognize = trimmed_audio.export(format='mp3').read()
     shazam = Shazam(mp3_file_content_to_recognize)
     recognize_generator = shazam.recognizeSong()
 
