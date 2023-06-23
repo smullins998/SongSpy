769ac05b2f8a5914d04f4420d4b3bcdd60a29b1e
Sean Mullins <Smullins998@gmail.com>
2023-06-23 18:36:18 -0400

    Update

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import os
 from werkzeug.utils import secure_filename
-import pytube
-from pytube import YouTube
-from pytube.exceptions import VideoUnavailable
+import yt_dlp
 from SVMFinal import extract_feature
 import time
 import ssl
@@ -12,6 +10,7 @@ import tempfile
 import io
 import pickle
 import json
+
 
 with open('mlp_model.pkl', 'rb') as file:
     model = pickle.load(file)
@@ -78,12 +77,30 @@ def youtube_upload():
         youtube_link = request.form.get('youtube-link')  # Access the value of the 'youtube-link' input field
         if youtube_link:
         
-            yt = pytube.YouTube(youtube_link)
-            stream = yt.streams.filter(only_audio=True).first()
-            filename = secure_filename(''.join(list(stream.default_filename)[0:-4]) + '.wav')
+            # yt = pytube.YouTube(youtube_link)
+            # stream = yt.streams.filter(only_audio=True).first()
+            # filename = secure_filename(''.join(list(stream.default_filename)[0:-4]) + '.wav')
            
-            stream.download(filename=filename)
+            # stream.download(filename=filename)
             
+
+
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+            }
+
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                info_dict = ydl.extract_info(youtube_link, download=True)
+                filename = ydl.prepare_filename(info_dict) 
+                filename = filename[0:-4] + 'mp3'
+                
+                                        
+                                        
             response = extract_feature(filename, model=model, scaler=scaler, SVM_key=SVM_key)
             
             try:
